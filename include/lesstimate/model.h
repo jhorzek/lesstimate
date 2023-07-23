c6b189d353be81946d6ecd2997af770acd8986cd
Jannik H. Orzek <orzek@mpib.berlin.mpg.de>
2023-07-23 11:24:39 +0200

    add default numerical approximation of gradients

--- include/lesstimate/model.h
+++ include/lesstimate/model.h
@@ -25,15 +25,41 @@ namespace lessSEM
                        stringVector parameterLabels) = 0;
 
     /**
-     * @brief gradients method with arguments parameterValues(arma::rowvec) and parameterLabels(stringVector; see common_headers.h) * specifying the parameter values and the labels of the paramters.The function should return the gradients(arma::rowvec)
+     * @brief gradients method with arguments parameterValues(arma::rowvec) and parameterLabels(stringVector; see common_headers.h) * specifying the parameter values and the labels of the paramters. The function should return the gradients(arma::rowvec).
+     * By default, a central gradient approximation with step size 1e-5 is used
      *
      * @param parameterValues numericVector with parameter values
      * @param parameterLabels stringVector with parameterLabels
      * @return arma::rowvec gradients
      */
     virtual arma::rowvec gradients(arma::rowvec parameterValues,
-                                   stringVector parameterLabels) = 0;
+                                   stringVector parameterLabels)
+    {
+      arma::rowvec gradients(paramterValues.n_elem);
+      gradients.fill(arma::darum::zeros);
+      for (unsigned int = 0; i < parameterValues.n_elem; i++)
+      {
+
+        // step forward
+        parameterValues(i) += stepSize;
+        gradients(i) = fit(parameterValues,
+                           parameterLabels);
+
+        // step backward
+        parameterValues(i) -= 2.0 * stepSize;
+        gradients(i) -= fit(parameterValues,
+                            parameterLabels);
+        // reset
+        parameterValues(i) += stepSize;
+
+        // compute gradient
+        gradients(i) /= 2.0 * stepSize;
+      }
+      return(gradients);
+    }
   };
 
+  // define stepSize used in numerically approximated gradients:
+  double stepSize = 1e-5;
 }
 #endif
