6a2801a995899d915289268a4d569388de54a361
Jannik <orzek@mpib-berlin.mpg.de>
2023-08-28 16:46:18 +0200

    using cpm.cmake

--- cmake/install-config.cmake
+++
@@ -1,6 +0,0 @@
-# This file was generated with cmake-init by friendlyanon at https://github.com/friendlyanon/cmake-init
-
-include(CMakeFindDependencyMacro)
-find_dependency(fmt)
-
-include("${CMAKE_CURRENT_LIST_DIR}/lesstimateTargets.cmake")
--- cmake/project-is-top-level.cmake
+++
@@ -1,8 +0,0 @@
-# This file was generated with cmake-init by friendlyanon at https://github.com/friendlyanon/cmake-init
-
-# This variable is set by project() in CMake 3.21+
-string(
-    COMPARE EQUAL
-    "${CMAKE_SOURCE_DIR}" "${PROJECT_SOURCE_DIR}"
-    PROJECT_IS_TOP_LEVEL
-)
